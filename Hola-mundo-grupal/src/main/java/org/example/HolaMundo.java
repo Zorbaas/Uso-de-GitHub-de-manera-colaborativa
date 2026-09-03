04fd17443ab58692e8334c9e192baf53a3b17e80
CaritaFliz <christian.sebastia10@gmail.com>
2026-09-02 22:43:22 -0400

    Subir mi HolaMundo.java

--- Hola-mundo-grupal/src/main/java/org/example/HolaMundo.java
+++ Hola-mundo-grupal/src/main/java/org/example/HolaMundo.java
@@ -1,5 +1,7 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("Benjamin Caripan");
+public class HolaMundo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola Mundo!!!!");
     }
 }
